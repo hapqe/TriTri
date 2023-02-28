f9438a65ac5cc3417c8f211c9550a85f1bf1f91f
Konrad Hapke <hap19520@spengergasse.at>
2023-02-28 15:23:06 +0100

    delete native files

--- Assets/Logic/NativeVersion.cs
+++ Assets/Logic/NativeVersion.cs
@@ -4,7 +4,7 @@ namespace Sc.Native
     public static class Version
     {
         #if UNITY_EDITOR
-        public const string version = "sc_native_1677592955";
+        public const string version = "sc_native_1677594166";
         #else
         public static string version = "sc_native";
         #endif
