e2b3b8262b657925c2269c7ba06a673310b4690e
Konrad Hapke <hap19520@spengergasse.at>
2023-02-28 15:24:36 +0100

    now?

--- Assets/Logic/NativeVersion.cs
+++ Assets/Logic/NativeVersion.cs
@@ -4,7 +4,7 @@ namespace Sc.Native
     public static class Version
     {
         #if UNITY_EDITOR
-        public const string version = "sc_native_1677594166";
+        public const string version = "sc_native_1677594264";
         #else
         public static string version = "sc_native";
         #endif
